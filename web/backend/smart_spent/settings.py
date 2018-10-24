a021280f5a3b39ad844e190c93eb48e2278f9c92
Zoran Altiparmakov <zoran.altiparmakov@gmail.com>
2018-10-24 02:17:18 +0200

    travis, docker fixes

--- web/backend/smart_spent/settings.py
+++ web/backend/smart_spent/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
